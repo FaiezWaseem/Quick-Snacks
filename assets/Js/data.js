3500eebe7d2ef5a8d7a6aee21bb332c71c6cc9f2
FaiezWaseem <78906502+FaiezWaseem@users.noreply.github.com>
2021-08-19 17:39:32 +0500

    created project

---
+++ assets/Js/data.js
@@ -0,0 +1,339 @@
+const snack_for_kids = {
+    Toddler_Muffins:{
+        link : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F749219.jpg&w=596&h=399&c=sc&poi=face&q=85",
+        preperation_Time : "30min",
+        serving : 24,
+        nutrion : "Per Serving: 89 calories; protein 1.6g; carbohydrates 11.8g; fat 4.5g; cholesterol 23.8mg; sodium 140.6mg.",
+        ingredient : [
+              "* ½ cup butter, softened",
+              "* ½ cup brown sugar, or to taste",
+              "* 2 large bananas, mashed",
+              "* 1 (4.5 ounce) jar baby food squash" ,
+              "* 2 carrots, grated" ,
+              "* 2 eggs, beaten" ,
+              "* 1 cup all-purpose flour" ,
+              "* ½ cup oat bran",
+              "* 1 teaspoon baking soda" , 
+              "* 1 teaspoon pumpkin pie spice",
+              "* ½ teaspoon salt"
+        
+      ],
+        cooking_des : {
+       step1 : 'Preheat an oven to 375 degrees F (190 degrees C). Grease 24 mini muffin cups or 12 standard muffin cups.',
+        step2 : 'In a mixing bowl, cream together the butter and brown sugar until smooth. Mix in the mashed bananas, squash, carrots, and eggs. Stir in the flour, oat bran, baking soda, pumpkin pie spice, and salt until just combined. Spoon the batter equally into the prepared muffin cups.',
+        step3: 'Bake in the preheated oven until a toothpick inserted into the center comes out clean, 15 to 20 minutes. Cool in the pans for 10 minutes before removing to cool completely on a wire rack. Store at room temperature for up to two days, or freeze.' 
+      },
+      purchase : "https://www.allrecipes.com/recipe/219330/toddler-muffins/"
+            
+       },
+    Kettle_Corn:{
+        link : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F18714.jpg&w=596&h=596&c=sc&poi=face&q=85",
+        preperation_Time : "20min",
+        serving : 5,
+        nutrion : "Per Serving: 209 calories; protein 2.4g; carbohydrates 24.8g; fat 11.9g; sodium 0.6mg.",
+        ingredient : [
+              "* ¼ cup vegetable oil",
+              "* ¼ cup white sugar",
+              "* ½ cup unpopped popcorn kernels"
+        
+      ],
+        cooking_des : {
+       step1 : 'Heat the vegetable oil in a large pot over medium heat. Once hot, stir in the sugar and popcorn. Cover, and shake the pot constantly to keep the sugar from burning.',
+        step2 : ' Once the popping has slowed to once every 2 to 3 seconds, remove the pot from the heat and continue to shake for a few minutes until the popping has stopped. Pour into a large bowl, and allow to cool, stirring occasionally to break up large clumps.' 
+      },
+      purchase : "https://www.allrecipes.com/recipe/20808/kettle-corn/"
+            
+       },
+    Watermelon_Ice_Pop :{
+        link : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F2271994.jpg&w=596&h=399&c=sc&poi=face&q=85",
+        preperation_Time : "2 hrs 10 mins",
+        serving : 6,
+        nutrion : "Per Serving: 31 calories; protein 0.3g; carbohydrates 8.1g; fat 0.1g; sodium 1.1mg.",
+        ingredient : [
+              "* 1 ½ cups watermelon, seeded and diced",
+              "* ½ cup water",
+              "* 1 tablespoon honey",
+              "* 1 tablespoon fresh lemon juice",
+              "* 1 tablespoon white sugar"
+      ],
+        cooking_des : {
+       step1 : 'Blend watermelon, water, honey, lemon juice, and sugar together in a blender until smooth. Pour mixture into ice pop molds and freeze until solid, about 2 hours.',
+        step2 : 'Run hot water over ice pop molds for a few seconds to unmold.' 
+      },
+      purchase : "https://www.allrecipes.com/recipe/242400/watermelon-ice-pops/"
+            
+       },
+    Fried_Flour_Tortilla_Chips:{
+        link : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F2613227.jpg&w=596&h=399&c=sc&poi=face&q=85",
+        preperation_Time : "20 mins",
+        serving : 8,
+        nutrion : "Per Serving: 243 calories; protein 5.8g; carbohydrates 35.9g; fat 8.2g; sodium 445.2mg.",
+        ingredient : [
+              "* vegetable oil for frying",
+              "* 8 (10 inch) flour tortillas, each cut into 8 triangles"
+      ],
+        cooking_des : {
+       step1 : 'Pour 1/4 to 1/2 inch oil in a heavy 10-inch skillet; bring oil to 360 degrees F (182 degrees C) over medium-high heat, about 8 minutes.',
+        step2 : 'Working in batches, gently arrange tortillas in the hot oil using a slotted spoon; cook, frequently turning tortillas with the slotted spoon, until golden brown, 2 to 3 minutes. Remove chips with slotted spoon and place a paper towel-lined plate to drain.' 
+      },
+      purchase : "https://www.allrecipes.com/recipe/244540/fried-flour-tortilla-chips/"
+            
+       }
+}
+const healthy_snacks ={
+    Energy_Meat_Balls :{
+        link : "https://www.eatthis.com/wp-content/uploads/sites/4/2017/07/energy-ball-bites-snack-dates-oats-fruit-nuts.jpg?resize=640,468&quality=82&strip=all",
+        preperation_Time : "15min",
+        serving : 30,
+        nutrion : "Per Serving: 146 calories; protein 5.6g; carbohydrates 15.5g; fat 7.9g; cholesterol 6mg; sodium 55mg",
+        ingredient : [
+              "* 2 cups rolled oats",
+              "* 1 cup peanut butter",
+              "* ⅔ cup honey",
+              "* ½ cup dark chocolate chips" ,
+              "* ½ cup ground flax seeds" ,
+              "* ½ cup chocolate-flavored protein powder" ,
+              "* ½ cup unsweetened shredded coconut" ,
+              "* 2 tablespoons chia seeds ",
+              "* 1 teaspoon coconut extract" , 
+              "* ¼ cup chopped coffee beans ",
+              "* 2 tablespoons cocoa powder (such as Fry's®), or more as needed "
+        
+      ],
+        cooking_des : {
+       step1 : 'Mix oats, peanut butter, honey, dark chocolate chips, flax seeds, protein powder, coconut, chia seeds, and coconut extract together in a large bowl; fold in coffee beans. Form mixture into 1-inch balls.',
+        step2 : 'Place cocoa powder in a small bowl. Roll each ball in the cocoa powder until coated.',
+        note: 'Vanilla protein powder can be used in placed of the chocolate protein powder. Semisweet chocolate chips can be used in place of the dark chocolate chips.' 
+      },
+      purchase : "https://www.allrecipes.com/recipe/256700/high-protein-energy-bites/#nutrition"
+            
+       },
+    Almond_Butter:{
+        link : "/images/almond.jpg",
+        preperation_Time : "10min",
+        serving : 16,
+        nutrion : "Per Serving: 57 calories; protein 1.9g; carbohydrates 1.8g; fat 5.1g; sodium 0.1mg.",
+        ingredient : [
+              "* 1 cup toasted almonds",
+              "* 2 teaspoons olive oil"
+        
+      ],
+        cooking_des : {
+       step1 : 'Place the almonds in a food processor. Process on high until ground almonds begin to form a ball. Drizzle olive oil over almonds and continue to process, stopping occasionally to scrape sides of the bowl with a spatula as needed. Store in an airtight container.'
+      },
+      purchase : "https://www.allrecipes.com/recipe/145449/almond-butter/"
+            
+       },
+    Tuna_Ball:{
+        link : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F703387.jpg&w=596&h=399&c=sc&poi=face&q=85",
+        preperation_Time : "15min",
+        serving : 20,
+        nutrion : "Per Serving: 66 calories; protein 2.6g; carbohydrates 1g; fat 6g; cholesterol 11.2mg; sodium 28.4mg",
+        ingredient : [
+              "* 2 (3 ounce) packages cream cheese, softened",
+              "* 1 (5 ounce) can tuna, drained and flaked",
+              "* ¾ cup chopped pecans (Optional)",
+              "* ¼ cup chopped onion"
+      ],
+        cooking_des : {
+       step1 : 'In a medium bowl, blend cream cheese, tuna, 1/2 of the pecans, and onion. Shape into a ball or loaf. Coat with remaining pecans. Refrigerate until serving.'
+      },
+      purchase : "https://www.allrecipes.com/recipe/24909/tuna-ball/"
+            
+       },
+     Hard_Boiled_Egg_Sandwich:{
+        link : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F9310274.jpg&q=85",
+        preperation_Time : "10min",
+        serving : 1,
+        nutrion : "Per Serving: 372 calories; protein 11.2g; carbohydrates 26.9g; fat 24.4g; cholesterol 262.2mg; sodium 860mg",
+        ingredient : [
+              "* 2 slices white bread, toasted",
+              "* 1 tablespoon butter, softened",
+              "* 2 tablespoons whipped cream cheese",
+              "* 1 hard-boiled egg, sliced",
+              "* 5 dashes hot pepper sauce (such as Frank's RedHot®), or to taste",
+              "* 1 pinch salt and ground black pepper to taste"
+      ],
+        cooking_des : {
+       step1 : 'Butter each slice of toast. Spread 1 tablespoon cream cheese onto each slice.',
+       step2 : 'Layer hard-boiled egg slices evenly onto 1 slice of toast. Add hot sauce, salt, and pepper. Top with other slice of toast.',
+        note: 'Substitute your favorite bread for the white bread, if you prefer.'
+      },
+      purchase : "https://www.allrecipes.com/recipe/274775/hard-boiled-egg-sandwich/"
+       }     
+}
+const easy_on_stomach ={
+    Moroccan_Vegetable_Soup:{
+        link : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F1046259.jpg&w=596&h=399&c=sc&poi=face&q=85",
+        preperation_Time : "1 hr 30min",
+        serving : 5,
+        nutrion : "147 calories; protein 2.3g; carbohydrates 17.8g; fat 8g; sodium 625.3mg.",
+        ingredient : [
+              "* 2 tablespoons olive oil (Optional)",
+              "* 1 onion, chopped",
+              "* 1 cup peeled, chopped carrots",
+              "* 1 cup peeled, chopped parsnips" ,
+              "* 1 cup canned pumpkin puree" ,
+              "* 1 quart vegetable stock" ,
+              "* 1 teaspoon lemon juice" ,
+              "* salt to taste",
+              "* ground black pepper to taste" , 
+              "* ½ teaspoon dried cilantro",
+              "* 2 teaspoons olive oil (Optional)",
+              "* 1 clove garlic, minced (Optional)",
+              "* 3 tablespoons chopped fresh parsley (Optional)",
+              "* ⅛ teaspoon paprika (Optional)" 
+      ],
+        cooking_des : {
+       step1 : 'Heat 2 tablespoons olive oil in a large pot. Stir in onions; cook and stir until the onion has softened and turned translucent, about 5 minutes. Add carrots and parsnips. Cover and cook until vegetables have softened slightly, about 5 minutes.',
+        step2 : 'Stir in the pumpkin puree and stock. Bring to a simmer and reduce heat to low. Cover and let simmer until vegetables are tender, about 40 minutes. Add lemon juice, salt, pepper, and cilantro. Taste and adjust seasonings, if necessary. Remove from heat and allow to cool.',
+        step3: 'Ladle soup in batches into a food processor and blend until smooth (or use an immersion blender). If soup is too thick, thin with additional stock. Return pureed soup to the pot and heat through.',
+       step4: 'To make the garnish, heat olive oil in a small skillet. Cook garlic and parsley over low heat 1 to 2 minutes. Stir in paprika. Swirl a half teaspoon of garnish on each serving of soup.' 
+      },
+      purchase : "https://www.allrecipes.com/recipe/171110/moroccan-vegetable-soup/"
+            
+       },
+    Indian_Vegetable_Rice:{
+        link : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F3620638.jpg&w=596&h=596&c=sc&poi=face&q=85",
+        preperation_Time : "50min",
+        serving : 2,
+        nutrion : "515 calories; protein 9.9g; carbohydrates 87.1g; fat 15.4g; sodium 904.6mg.",
+        ingredient : [
+              "* 2 tablespoons vegetable oil",
+              "* 1 onion, thinly sliced",
+              "* ½ teaspoon ground cumin",
+              "* 1 cup Basmati rice" ,
+              "* 2 cups water" ,
+              "* ¾ teaspoon salt" ,
+              "* ½ teaspoon garam masala" ,
+              "* ¾ cup frozen mixed vegetables"
+      ],
+        cooking_des : {
+       step1 : 'In a large pan heat oil over medium heat and saute onions and cumin until onions are tender, but not browned. Rinse the rise several times and drain well. Add the rice to the pan with the onions and pour in the 2 cups of water. Stir in salt, garam masala and vegetables. Cover the pan and increase the heat to high.',
+        step2 : 'Bring the mixture to a boil and then reduce the heat to low, keeping the rice covered at all times. After cooking for about 10 minutes, gently stir the rice to distribute the spices evenly. Cook 25 to 30 minutes, or until all of the water has evaporated and the rice is tender.'
+       },
+      purchase : "https://www.allrecipes.com/recipe/21003/indian-vegetable-rice/"
+            
+       },
+    Mixed_Vegetable_Salad:{
+        link : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F935859.jpg&w=596&h=596&c=sc&poi=face&q=85",
+        preperation_Time : "6 hr 35mins",
+        serving : 8,
+        nutrion : "139 calories; protein 3.7g; carbohydrates 31.9g; fat 0.4g; sodium 162.8mg.",
+        ingredient : [
+              "* 8 ounces mixed vegetables",
+              "* 1 (15 ounce) can red beans, drained and rinsed",
+              "* ¼ cup chopped onion",
+              "* ¼ cup chopped celery" ,
+              "* ¼ cup chopped green bell pepper" ,
+              "* ½ cup white wine vinegar" ,
+              "* ¾ cup white sugar" ,
+              "* 1 teaspoon prepared mustard",
+              "* 1 tablespoon all-purpose flour"
+      ],
+        cooking_des : {
+       step1 : 'In a mixing bowl, combine the mixed vegetables, beans, onion, celery and bell pepper.',
+        step2 : 'In a small sauce pan, combine vinegar, sugar, mustard and flour. Bring to boil, remove from heat and set aside to cool.',
+        step3: 'Pour dressing over vegetables and toss. Refrigerate for at least 6 hours and serve.'
+      },
+      purchase : "https://www.allrecipes.com/recipe/14355/mixed-vegetable-salad-ii/"
+            
+       },
+    Strawberry_Apple_Sauce:{
+        link : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F5335325.jpg&w=596&h=596&c=sc&poi=face&q=85",
+        preperation_Time : "18 mins",
+        serving : 4,
+        nutrion : "Per Serving: 42 calories; protein 0.3g; carbohydrates 10.9g; fat 0.2g; sodium 0.9mg.",
+        ingredient : [
+              "* 2 apples, cored and sliced into 8 wedges",
+              "* 4 strawberries, quartered, or more to taste"
+      ],
+        cooking_des : {
+       step1 : 'Place apples in a microwave-safe container; cover and cook in the microwave until tender, about 2 minutes. Add strawberries; cover and cook in the microwave for 1 minute more.',
+        step2 : 'Transfer apples and strawberries to a blender; pulse until smooth and mixture reaches a consistency similar to yogurt or applesauce.'
+      },
+      purchase : "https://www.allrecipes.com/recipe/247146/strawberry-apple-baby-food/"
+            
+       }
+}
+const smoothie = {
+    Avo_Berry_Smoothie:{
+        link : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F6864534.jpg&w=596&h=399&c=sc&poi=face&q=85",
+        preperation_Time : "10 min",
+        serving : 1,
+        nutrion : "Per Serving: 416 calories; protein 18.5g; carbohydrates 50.3g; fat 18g; sodium 229.8mg.",
+        ingredient : [
+              "* 2 scoops Emergen-C® Protein Fuel and Super Foods Mixed Berry",
+              "* 1 cup frozen blueberries",
+              "* ½ cup orange juice",
+              "* ½ ripe avocado, peeled and pitted" 
+      ],
+        cooking_des : {
+       step1 : 'Place all ingredients in a blender. Blend until smooth. Serve and enjoy!'
+      },
+      purchase : "https://www.allrecipes.com/recipe/274880/avo-berry-smoothie/"
+            
+       },
+    Mango_Smoothie:{
+        link : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F420632.jpg&w=596&h=596&c=sc&poi=face&q=85",
+        preperation_Time : "10 min",
+        serving : 4,
+        nutrion : "Per Serving: 395 calories; protein 3.1g; carbohydrates 28.7g; fat 32.1g; cholesterol 40.8mg; sodium 26.6mg.",
+        ingredient : [
+              "* 1 mango - peeled, seeded, and cubed",
+              "* 1 ¼ cups ice cubes",
+              "* 2 tablespoons white sugar",
+              "* 1 ¼ cups pineapple juice", 
+              "* ½ cup heavy cream", 
+              "* 1 (14 ounce) can coconut milk"
+      ],
+        cooking_des : {
+       step1 : 'Place the mango cubes, ice, sugar, pineapple juice, cream, and coconut milk into a blender. Puree until smooth, pour into glasses and serve.'
+      },
+      purchase : "https://www.allrecipes.com/recipe/172367/mango-pina-colada-smoothie/"
+            
+       },
+    Banana_Berry_Smoothie:{
+        link : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F3214678.jpg&w=596&h=399&c=sc&poi=face&q=85",
+        preperation_Time : "10 min",
+        serving : 2,
+        nutrion : "Per Serving: 249 calories; protein 3.7g; carbohydrates 60.1g; fat 1g; cholesterol 1.5mg; sodium 23mg.",
+        ingredient : [
+              "* 1 banana, frozen and chunked",
+              "* 1 cup frozen raspberries",
+              "* ¾ cup orange juice",
+              "* ¼ cup vanilla yogurt"
+      ],
+        cooking_des : {
+       step1 : 'In a blender, combine banana, raspberries, orange juice and yogurt. Blend until smooth. Pour into glasses and serve.'
+      },
+      purchase : "https://www.allrecipes.com/recipe/32560/banana-berry-smoothie-iii/ "
+            
+       },
+    Keto_Chocolate_Smoothie:{
+        link : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F6389126.jpg&w=596&h=596&c=sc&poi=face&q=85",
+        preperation_Time : "15 min",
+        serving : 2,
+        nutrion : "Per Serving: 498 calories; protein 6.7g; carbohydrates 18.2g; fat 48.1g; sodium 71.9mg.",
+        ingredient : [
+              "* 1 tablespoon chia seeds",
+              "* 3 tablespoons water",
+              "* 1 cup full-fat coconut milk",
+              "* ½ frozen avocado",
+              "* 1 tablespoon peanut butter",
+              "* 1 tablespoon coconut oil",
+              "* 2 teaspoons unsweetened cocoa powder",
+              "* ½ cup ice cubes (Optional)",
+              "* ¼ cup water",
+              "* 2 tablespoons cocoa nibs",
+              "* 1 teaspoon ground cinnamon"
+      ],
+        cooking_des : {
+       step1 : 'Soak chia seeds in 3 tablespoons water for 10 minutes.',
+       step2:'Combine coconut milk, avocado, peanut butter, chia seeds, coconut oil, cocoa powder, ice cubes, and 1/4 cup water in a blender. Blend until smooth. Top with cocoa nibs and cinnamon.'
+      },
+      purchase : "https://www.allrecipes.com/recipe/264207/keto-chocolate-smoothie/"
+            
+       }
+} 
+export {snack_for_kids , healthy_snacks , easy_on_stomach , smoothie};
